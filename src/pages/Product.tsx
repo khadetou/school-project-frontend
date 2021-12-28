84bed60f8fb0eaeeb0d1a984ec83141bc7ae2fae
khadetou <khadetou96@gmail.com>
2021-12-28 09:27:15 +0000

    display all product with filters

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -3,6 +3,7 @@ import { arrowRedoOutline, cart, cartOutline, chevronBackOutline, heart, heartOu
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router";
 import { useAction } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypeSelector";
 import './Product.css';
 
 
@@ -10,33 +11,7 @@ import './Product.css';
 const Product: React.FC = () => {
     const [isFavorite, setIsFavorite] = useState(false);
 
-    const product = {
-        img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-        name: "product1",
-        price: 10
-    };
-    const products = [
-        {
-            img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            name: "product1",
-            price: 10
-        },
-        {
-            img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            name: "product2",
-            price: 20
-        },
-        {
-            img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            name: "product3",
-            price: 30
-        },
-        {
-            img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-            name: "product4",
-            price: 40
-        }
-    ]
+
     const { getProducts } = useAction();
     //@ts-ignore
     const { category } = useParams();
@@ -70,7 +45,14 @@ const Product: React.FC = () => {
         }
     }, [])
 
-
+    const { data, error, loading } = useTypedSelector(state => state.getProducts);
+    //Reduce the length of the name of data to display
+    const products = data!.map(product => {
+        return {
+            ...product,
+            name: product.name.substring(0, 20) + "..."
+        }
+    });
 
     return (
         <IonPage>
@@ -92,190 +74,30 @@ const Product: React.FC = () => {
                 <IonGrid>
                     <IonRow>
                         <IonCol size="12" >
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
-                            <IonCard className="category-card">
-                                <IonCardHeader className="product-card-header">
-                                    <div className="product-card-actions">
-                                        <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
-                                        <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
-                                        <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
-                                    </div>
-                                    <img src={product.img} alt={product.name} />
-                                    <p className="ion-text-wrap">{product.name}</p>
-                                </IonCardHeader>
-                                <IonCardContent className="category-card-content">
-                                    <div className="product-price">
-                                        <IonButton color="light" size="large">
-                                            $ {product.price}
-                                        </IonButton>
-                                        <IonButton size="large" color="dark">
-                                            <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
-                                        </IonButton>
-                                        <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
-                                    </div>
-                                </IonCardContent>
-                            </IonCard>
-
+                            {data && products.map((product) => (
+                                <IonCard className="category-card" key={product.id}>
+                                    <IonCardHeader className="product-card-header">
+                                        <div className="product-card-actions">
+                                            <IonIcon className="product-card-action" color={isFavorite ? "danger" : "medium"} icon={isFavorite ? heart : heartOutline} onClick={() => setIsFavorite(!isFavorite)} />
+                                            <IonIcon style={{ position: "absolute", display: "none" }} className={`product-card-action  animate__animated`} color="danger" icon={heart} />
+                                            <IonIcon className="product-card-action" size="medium" icon={arrowRedoOutline} />
+                                        </div>
+                                        <img src={product.image} alt={product.name} />
+                                        <p className="ion-text-wrap">{product.name}</p>
+                                    </IonCardHeader>
+                                    <IonCardContent className="category-card-content">
+                                        <div className="product-price">
+                                            <IonButton color="light" size="large">
+                                                $ {product.price}
+                                            </IonButton>
+                                            <IonButton size="large" color="dark">
+                                                <IonIcon icon={cartOutline} />&nbsp;&nbsp;Add to Cart
+                                            </IonButton>
+                                            <IonIcon icon={cart} color="dark" style={{ position: "absolute", display: "none", fontSize: "3rem" }} className="animate__animated" />
+                                        </div>
+                                    </IonCardContent>
+                                </IonCard>))
+                            }
                         </IonCol>
                     </IonRow>
                 </IonGrid>
